f5e06a85c7e7907a82b2efad5b968f297cb0395f
Rijan Adhikari <rijan89732@gmail.com>
2024-10-05 19:49:21 +0545

    Create login page and routes

--- latestproject/src/App.jsx
+++ latestproject/src/App.jsx
@@ -1,13 +1,20 @@
+import Login from "./components/Login"
 import Register from "./components/Register"
+import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
 
 
 function App() {
   
 
   return (
-    <>
-      <Register/>
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
--- latestproject/src/components/Login.jsx
+++ latestproject/src/components/Login.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  //using one usetstae to manage all form field
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  //destructuring form data for easy access
+  const { email, password } = formData;
+  const handleChange = (e) =>{
+    setFormData({
+      //spread operator to keep existing state
+      ...formData,
+      [e.target.name]: e.target.value  //it update the specific field
+    });
+    console.log(formData);
+
+  };
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log("Login details here", formData);
+
+  }
+
+
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input 
+          type="password"
+          id = "password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          required
+          />
+          <button type="submit" className="login-button">
+            Login
+          </button>
+
+
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
